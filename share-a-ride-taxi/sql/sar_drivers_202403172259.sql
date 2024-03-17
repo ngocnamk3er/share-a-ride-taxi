f62110aad712848f91f0316564d60097f9552efa
ngocnamk3er <ngocnamk3er@gmail.com>
2024-03-17 23:00:05 +0700

    them cac file sql

---
+++ share-a-ride-taxi/sql/sar_drivers_202403172259.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.sar_drivers (driver_id,full_name,phone_number,email,gender,vehicle_type,vehicle_license_plate,active,created_at) VALUES
+	 ('2f8eb389-a920-42b1-b681-443ba102b482','John Doe','1234567890','john.doe@example.com','Male','Car','ABC123',true,'2024-03-17 22:15:17.699512+07'),
+	 ('1a65bdfb-90b7-4a5f-adf1-9645541d33b4','Alice Smith','9876543210','alice.smith@example.com','Female','Truck','XYZ789',true,'2024-03-17 22:15:39.106875+07'),
+	 ('a5015326-083c-46a5-9ca2-6e97e7a759ee','Michael Johnson','5551234567','michael.johnson@example.com','Male','Motorcycle','DEF456',true,'2024-03-17 22:15:46.387646+07'),
+	 ('41d10a86-59ff-4cc4-a956-6e7a891bb285','Emily Davis','1239876543','emily.davis@example.com','Female','Van','GHI789',true,'2024-03-17 22:15:56.574951+07'),
+	 ('93d36cd8-33c4-4c46-b800-43328038aa4f','David Miller','9871234567','david.miller@example.com','Male','Car','JKL012',true,'2024-03-17 22:16:02.173424+07'),
+	 ('667f0d18-0874-4a13-bda1-82444d979e48','Sophia Wilson','5559876543','sophia.wilson@example.com','Female','Truck','MNO345',true,'2024-03-17 22:16:07.969483+07'),
+	 ('e9b40ed2-6894-4401-b051-9d46b9fdd036','Daniel Taylor','7891234567','daniel.taylor@example.com','Male','Car','PQR678',true,'2024-03-17 22:16:13.899828+07'),
+	 ('a6c513ab-caed-40a0-8ab9-53140df1b907','Olivia Brown','123456789010','olivia.brown@example.com','Female','Van','STU901',true,'2024-03-17 22:16:28.121451+07'),
+	 ('c527418d-8b09-4995-97fe-16f25945067c','James Lee','55598765431','james.lee@example.com','Male','Motorcycle','VWX234',true,'2024-03-17 22:17:05.140649+07'),
+	 ('84dc30a7-633b-4f99-903e-d5efdc757bcc','Emma Clark put 1st','1239876542','emma.clark@example.com','Female','Car','YZA345',true,'2024-03-17 22:17:49.761718+07');
---
+++ share-a-ride-taxi/sql/sar_request_status_202403172259.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.sar_request_status (status_name) VALUES
+	 ('Received'),
+	 ('Driver assigned'),
+	 ('In transit'),
+	 ('Delivered'),
+	 ('Cancelled');
