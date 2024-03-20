855bd58bd1fadbef3361fb16828365279595f668
ngocnamk3er <ngocnamk3er@gmail.com>
2024-03-21 02:53:34 +0700

    tim duong di giua 2 diem

--- share-a-ride-taxi/openerp-oauth2-client/src/components/Map.jsx
+++ share-a-ride-taxi/openerp-oauth2-client/src/components/Map.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import RoutingMachine from "./RoutingMachine";
 
 const icon = L.icon({
     iconUrl: "../../../placeholder.png",
@@ -11,17 +12,17 @@ const icon = L.icon({
 const Map = (props) => {
     const pickupLocation = props.pickupLocation;
     const pickupPosition = [pickupLocation.lat, pickupLocation.lon];
-    
+    const myMap = useRef(null);
     const dropoffLocation = props.dropoffLocation;
     const dropoffPosition = [dropoffLocation.lat, dropoffLocation.lon];
 
     return (
-        <MapContainer style={{ width: "100%", height: "80vh" }} center={pickupPosition} zoom={8} scrollWheelZoom={true}>
+        <MapContainer ref={myMap} style={{ width: "100%", height: "80vh" }} center={pickupPosition} zoom={8} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=S2AoXO1T66lM1OcyaD9F"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={pickupPosition} icon={icon}>
+            {/* <Marker position={pickupPosition} icon={icon}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
@@ -30,7 +31,8 @@ const Map = (props) => {
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
-            </Marker>
+            </Marker> */}
+            <RoutingMachine pickupLocation={pickupLocation} dropoffLocation={dropoffLocation} />
         </MapContainer>
     );
 }
