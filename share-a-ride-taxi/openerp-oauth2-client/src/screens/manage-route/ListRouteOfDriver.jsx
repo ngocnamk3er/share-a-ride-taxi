b30618094ddac5160e31b6666c2c5d917ab2b2de
ngocnamk3er <ngocnamk3er@gmail.com>
2024-04-19 02:00:29 +0700

    sua lai man hinh list route of driver

--- share-a-ride-taxi/openerp-oauth2-client/src/screens/manage-route/ListRouteOfDriver.jsx
+++ share-a-ride-taxi/openerp-oauth2-client/src/screens/manage-route/ListRouteOfDriver.jsx
@@ -4,57 +4,95 @@ import { useParams, useHistory } from "react-router-dom";
 import { StandardTable } from "erp-hust/lib/StandardTable";
 import { request } from "../../api";
 import { useRouteMatch } from 'react-router-dom';
+import { format, isSameDay, isAfter } from 'date-fns';
+import { Button } from "@mui/material";
 
 const ListRouteOfDriver = () => {
-    const [routes, setRoutes] = useState([]);
+    const [routeGroups, setRouteGroups] = useState([]);
     const [driverName, setDriverName] = useState([]);
     const { id } = useParams();
     const history = useHistory();
     let { path } = useRouteMatch();
+    const [showAll, setShowAll] = useState(false);
+    const [showExpandButton, setShowExpandButton] = useState(true);
 
     useEffect(() => {
         request("get", `/routes/search?driverId=${id}`, (res) => {
-            setRoutes(res.data.reverse());
+            const routes = res.data.reverse();
+            const groupedRoutes = groupRoutesByDate(routes);
+            setRouteGroups(groupedRoutes);
         }).then();
 
         request("get", `/drivers/${id}`, (res) => {
             setDriverName(res.data.fullName);
         }).then();
 
-    }, [id]); // Add id to the dependency array
+    }, [id]);
 
-    const handleRowClick = (event, rowData) => {
-        const currentPath = history.location.pathname; // Lấy đường dẫn hiện tại
-        const newPath = `${currentPath}/${rowData.id}`; // Tạo đường dẫn mới với rowData.id
-        history.push(newPath); // Chuyển hướng đến đường dẫn mới
+    const groupRoutesByDate = (routes) => {
+        const groups = [];
+        routes.forEach((route) => {
+            const routeDate = new Date(route.startExecutionStamp).toDateString();
+            const existingGroupIndex = groups.findIndex(group => isSameDay(new Date(group.date), new Date(route.startExecutionStamp)));
+            if (existingGroupIndex !== -1) {
+                groups[existingGroupIndex].routes.push(route);
+            } else {
+                groups.push({ date: routeDate, routes: [route] });
+            }
+        });
+        return groups;
     };
 
-    const columns = [
-        {
-            title: "Tilte ",
-            field: "title",
-        },
-        {
-            title: "Description",
-            field: "description",
-        },
-    ];
+    const handleRowClick = (event, rowData) => {
+        const currentPath = history.location.pathname;
+        const newPath = `${currentPath}/${rowData.id}`;
+        history.push(newPath);
+    };
+
+    const toggleShowAll = () => {
+        setShowAll(!showAll);
+        // setShowExpandButton(!showExpandButton);
+    };
+
+    const renderGroupedRoutes = () => {
+        return routeGroups.map((group, index) => {
+            const currentDate = new Date(group.date);
+            const isPastDate = isAfter(new Date(), currentDate);
+            const shouldShow = showAll || !isPastDate;
+            return (
+                <div key={index}>
+                    <h2>{group.date}</h2>
+                    {shouldShow && (
+                        <StandardTable
+                            title={`Routes on ${group.date}`}
+                            columns={[
+                                { title: "Tilte", field: "title" },
+                                { title: "Start execution time", field: "startExecutionStamp", render: rowData => format(new Date(rowData.startExecutionStamp), "'Ngày' dd/MM/yyyy 'lúc' HH:mm") },
+                                { title: "Description", field: "description" }
+                            ]}
+                            data={group.routes}
+                            onRowClick={handleRowClick}
+                            options={{
+                                selection: false,
+                                pageSize: 3,
+                                search: true,
+                                sorting: true,
+                            }}
+                        />
+                    )}
+                </div>
+            );
+        });
+    };
 
     return (
         <div>
-            <StandardTable
-                title={`List routes of Driver ${driverName}`}
-                columns={columns}
-                data={routes}
-                onRowClick={handleRowClick}
-                // hideCommandBar
-                options={{
-                    selection: false,
-                    pageSize: 20,
-                    search: true,
-                    sorting: true,
-                }}
-            />
+            {renderGroupedRoutes()}
+            <br />
+            <br />
+            {showExpandButton && (
+                <Button style={{ backgroundColor: 'blue', color: 'white' }} onClick={toggleShowAll}>{showAll ? "Thu gọn" : "Xem thêm"}</Button>
+            )}
         </div>
     );
 }
--- share-a-ride-taxi/openerp-resource-server/src/main/java/openerp/openerpresourceserver/controller/DemoController.java
+++ share-a-ride-taxi/openerp-resource-server/src/main/java/openerp/openerpresourceserver/controller/DemoController.java
@@ -1,31 +1,13 @@
 package openerp.openerpresourceserver.controller;
 
+import lombok.RequiredArgsConstructor;
 import openerp.openerpresourceserver.enums.RequestType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class DemoController {
-
-    @GetMapping("/hi")
-    public String home() {
-        return "Welcome to the demo application!";
-    }
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, world!";
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("hasRole('WMS_ONLINE_CUSTOMER')")
-    public String admin() {
-        System.out.println("------------");
-        System.out.println(RequestType.passenger.toString());
-        System.out.println(RequestType.passenger);
-        System.out.println("------------");
-        return "This is an admin endpoint";
-    }
 
 }
