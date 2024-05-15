c74383857ade07367178145b7e025ce145f95e18
ngocnamk3er <ngocnamk3er@gmail.com>
2024-05-16 03:32:35 +0700

    clean code

--- share-a-ride-taxi/openerp-resource-server/src/main/java/openerp/openerpresourceserver/service/Impl/Auto/AutoAssignService.java
+++ share-a-ride-taxi/openerp-resource-server/src/main/java/openerp/openerpresourceserver/service/Impl/Auto/AutoAssignService.java
@@ -25,19 +25,29 @@ public class AutoAssignService {
     private final ParcelRequestService parcelRequestService;
     private final WarehouseService warehouseService;
     private final GraphHopperCalculator graphHopperCalculator;
+    private final Vector<Pair<Warehouse, ParcelRequest>> vectorPickUp = new Vector<>();
+    private final Vector<Pair<Warehouse, ParcelRequest>> vectorDropOff = new Vector<>();
+    private final Vector<RoutePickupDetail> routePickupDetailVector = new Vector<>();
+    private final Vector<RoutePickup> vectorRoutePickup = new Vector<>();
+    private List<ParcelRequest> parcelPassengers;
+    private List<Warehouse> warehouses;
 
     public String autoAssign() throws Exception {
-        List<ParcelRequest> parcelPassengers = parcelRequestService.getAllParcelRequests();
-        List<Warehouse> warehouses = warehouseService.getAllWarehouses();
-        Vector<Pair<Warehouse, ParcelRequest>> vectorPickUp = new Vector<>();
-        Vector<Pair<Warehouse, ParcelRequest>> vectorDropOff = new Vector<>();
+        parcelPassengers = parcelRequestService.getAllParcelRequests();
+        warehouses = warehouseService.getAllWarehouses();
+
+        clusteringPickUp();
+
+        return routePickupDetailVector.stream()
+                .map(routePickupDetail -> "RouteId : " + routePickupDetail.getRouteId() + " RequestId: " + routePickupDetail.getRequestId() + " SedIndex : " + routePickupDetail.getSeqIndex())
+                .collect(Collectors.joining("\n"));
+    }
+
+    private void clusteringPickUp() throws Exception {
         for (ParcelRequest parcelRequest : parcelPassengers) {
             Coordinate parcelRequestPickUpLocation = new Coordinate(parcelRequest.getPickupLatitude(), parcelRequest.getPickupLongitude());
-            Coordinate parcelRequestDropOffLocation = new Coordinate(parcelRequest.getDropoffLatitude(), parcelRequest.getDropoffLongitude());
             Warehouse pickUpWarehouse = new Warehouse();
-            Warehouse dropOffWarehouse = new Warehouse();
-            Double distancePickUp = Double.MAX_VALUE;
-            Double distanceDropOff = Double.MAX_VALUE;
+            double distancePickUp = Double.MAX_VALUE;
             for (Warehouse warehouse : warehouses) {
                 Coordinate wareHouseLocation = new Coordinate(warehouse.getLat(), warehouse.getLon());
                 ResponsePath pickUpWarehousePath = graphHopperCalculator.calculate(parcelRequestPickUpLocation, wareHouseLocation);
@@ -45,22 +55,14 @@ public class AutoAssignService {
                     distancePickUp = pickUpWarehousePath.getDistance();
                     pickUpWarehouse = warehouse;
                 }
-
-                ResponsePath dropOffWarehousePath = graphHopperCalculator.calculate(wareHouseLocation, parcelRequestDropOffLocation);
-                if (dropOffWarehousePath.getDistance() < distanceDropOff) {
-                    distanceDropOff = dropOffWarehousePath.getDistance();
-                    dropOffWarehouse = warehouse;
-                }
             }
             vectorPickUp.add(Pair.of(pickUpWarehouse, parcelRequest));
-            vectorDropOff.add(Pair.of(dropOffWarehouse, parcelRequest));
         }
         vectorPickUp.sort(Comparator.comparing(pair -> pair.getFirst().getWarehouseId()));
 
         vectorDropOff.sort(Comparator.comparing(pair -> pair.getFirst().getWarehouseId()));
 
         Set<String> pickUpWareHouseName = new HashSet<>(); // Khởi tạo một Set rỗng để lưu trữ wareHouseId
-        Vector<RoutePickup> vectorRoutePickup = new Vector<>();
 
         for (Pair<Warehouse, ParcelRequest> pair : vectorPickUp) {
             pickUpWareHouseName.add(pair.getFirst().getWarehouseId()); // Thêm wareHouseId vào Set
@@ -75,8 +77,6 @@ public class AutoAssignService {
         }
 
 
-        Vector<RoutePickupDetail> routePickupDetailVector = new Vector<>();
-
         for (int i = 0; i < vectorPickUp.size(); i++) {
             Pair<Warehouse, ParcelRequest> pair = vectorPickUp.get(i);
             Warehouse warehousePickUp = pair.getFirst();
@@ -84,21 +84,10 @@ public class AutoAssignService {
             RoutePickupDetail routePickupDetail = RoutePickupDetail.builder()
                     .routeId(warehousePickUp.getWarehouseId() + "_pickup_route")
                     .requestId(parcelRequest.getRequestId())
-                    .seqIndex(i+1)
+                    .seqIndex(i + 1)
                     .build();
             routePickupDetailVector.add(routePickupDetail);
         }
-
-
-
-//        return vectorRoutePickup.stream()
-//                .map(routePickup -> "Id: " + routePickup.getId() + " WarehouseId: " + routePickup.getWareHouseId())
-//                .collect(Collectors.joining("\n"));
-
-        return routePickupDetailVector.stream()
-                .map(routePickupDetail -> "RouteId : " + routePickupDetail.getRouteId() + " RequestId: " + routePickupDetail.getRequestId() + " SedIndex : " + routePickupDetail.getSeqIndex())
-                .collect(Collectors.joining("\n"));
     }
-
 
 }
